2ab78f9b8a7b401d302a688239b12ec004d0df3b
lid <liduo95@gmail.com>
2019-01-24 01:31:04 +0800

    channels made divisible by 8, hardware friendly, remove dropout layer before classfier

--- models/imagenet/mobilenetv2.py
+++ models/imagenet/mobilenetv2.py
@@ -12,6 +12,26 @@ import math
 __all__ = ['mobilenetv2']
 
 
+def _make_divisible(v, divisor, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    :param v:
+    :param divisor:
+    :param min_value:
+    :return:
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
 def conv_3x3_bn(inp, oup, stride):
     return nn.Sequential(
         nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
@@ -85,12 +105,12 @@ class MobileNetV2(nn.Module):
 
         # building first layer
         assert input_size % 32 == 0
-        input_channel = int(32 * width_mult)
+        input_channel = _make_divisible(32 * width_mult, 8)
         layers = [conv_3x3_bn(3, input_channel, 2)]
         # building inverted residual blocks
         block = InvertedResidual
         for t, c, n, s in self.cfgs:
-            output_channel = int(c * width_mult)
+            output_channel = _make_divisible(c * width_mult, 8)
             layers.append(block(input_channel, output_channel, s, t))
             input_channel = output_channel
             for i in range(1, n):
@@ -98,10 +118,9 @@ class MobileNetV2(nn.Module):
                 input_channel = output_channel
         self.features = nn.Sequential(*layers)
         # building last several layers
-        output_channel = int(1280 * width_mult) if width_mult > 1.0 else 1280
+        output_channel = _make_divisible(1280 * width_mult, 8) if width_mult > 1.0 else 1280
         self.conv = conv_1x1_bn(input_channel, output_channel)
         self.avgpool = nn.AvgPool2d(input_size // 32, stride=1)
-        self.dropout = nn.Dropout(.2)
         self.classifier = nn.Linear(output_channel, num_classes)
 
         self._initialize_weights()
@@ -111,7 +130,6 @@ class MobileNetV2(nn.Module):
         x = self.conv(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        x = self.dropout(x)
         x = self.classifier(x)
         return x
 
